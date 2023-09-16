8e81be138df7a7116401bf4feaa20e5335337320
Mario Bianchi <bianchi.mario@outlook.com>
2023-09-16 15:57:41 +0200

    Fixed non-sliding windows

--- MARS/MARS.py
+++ MARS/MARS.py
@@ -9,27 +9,30 @@ class MARS:
         self.seed = seed
 
     def get_shapelets(self, time_series):
+        '''
+        Extracts multivariate shapelets
+        '''
         if self.seed is not None:
             random.seed(self.seed)
 
         num_dimensions = len(time_series[0])
-        max_window_length = len(time_series[0][0])
+        #max_window_length = len(time_series[0][0])
+        max_window_length = min([len(e) for e in time_series[0]]) # length of the shortes dimension
 
         if self.shapelet_length > max_window_length:
             raise ValueError("Shapelet length is greater than the length of the time series dimensions. "
                              "Please specify a valid shapelet length.")
 
-        sliding_windows = []
+        sliding_windows = [] # each element of sliding_windows represents a shapelet for a specific permutation
 
-        for _ in range(self.num_permutations):
-            shapelet = []
+        for _ in range(self.num_permutations): # for every shapelet created from a time series
+            shapelet = [] # it represents one set of sliding windows for all dimensions of the MTS
 
-            for dim_idx in range(num_dimensions):
+            for dim_idx in range(num_dimensions): # for every dimension
                 window_length = self.shapelet_length
-                if not self.same_start:
-                    window_indices = random.randint(0, max_window_length - window_length)
-                else:
-                    window_indices = 0
+                
+                window_indices = random.randint(0, max_window_length - window_length) # index from zero to last possible position
+                
                 window = [ts[dim_idx][window_indices:window_indices + window_length] for ts in time_series]
                 shapelet.append(window)
 
@@ -37,20 +40,29 @@ class MARS:
 
         return sliding_windows
 
+
     def calculate_shapelet_distance(self, shapelet, time_series):
+        '''
+        Calculates distance between a shapelet and a time series
+        '''
         min_distance = float('inf')
-
-        for shapelet_window, time_series_window in zip(shapelet, time_series):
+        shapelet_length = len(shapelet[0][0])
+        
+        for start_pos in range(len(time_series[0][0]) - shapelet_length + 1):
             total_distance = 0.0
-
-            for dim in range(len(shapelet_window)):
-                dimension_distance = np.linalg.norm(shapelet_window[dim] - time_series_window[dim])
+            
+            for dim in range(len(shapelet[0])):
+                dimension_distance = np.linalg.norm(  # calculates the Euclidean distance between the vectors a and b
+                    shapelet[0][dim] - time_series[0][dim][start_pos:start_pos + shapelet_length]
+                )
                 total_distance += dimension_distance
-
+            
             if total_distance < min_distance:
                 min_distance = total_distance
-
+        
         return min_distance
+
+
 
     def get_distances(self, shapelets, time_series_dataset):
         distances = []
