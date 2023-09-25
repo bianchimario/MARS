45d39d0001d4272600848ea4432a1b50b365b680
Mario Bianchi <bianchi.mario@outlook.com>
2023-09-25 15:44:28 +0200

    Add get_random_shapelets

--- MARS/MARS.py
+++ MARS/MARS.py
@@ -33,6 +33,39 @@ def get_shapelets(time_series_dataset, num_shapelets, len_shapelets, async_shape
     return shapelets
 
 
+def get_random_shapelets(time_series_dataset, num_shapelets, max_len, min_len, async_shapelets=True, seed=None):
+    if seed is not None:
+        random.seed(seed)
+
+    dims = len(time_series_dataset[0])
+    max_possible_length = min([len(e) for e in time_series_dataset[0]]) # length of the shortest dimension
+
+    if (max_len>max_possible_length) or (min_len>max_possible_length):
+        raise ValueError("Shapelet length is greater than the length of the shortest dimension.")
+    
+    shapelets = []
+    random_ts = random.sample(range(0, len(time_series_dataset)), num_shapelets) # indexes of the time series that generate a shapelet
+
+    for idx in random_ts:
+        ts = time_series_dataset[idx]
+
+        if async_shapelets:
+            single_shapelet = []
+            for dim in range(0,dims): # async: new starting point for each dimension
+                    random_length = random.randint(min_len,max_len)
+                    start_idx = random.randint(0,len(ts[dim]) - random_length)
+                    single_shapelet.append(ts[dim][start_idx:start_idx+random_length])
+            shapelets.append(single_shapelet)
+
+        else:
+            random_length = random.randint(min_len,max_len)
+            start_idx = random.randint(0, max_len - random_length) # index from zero to last possible position
+            single_shapelet = [ts[dim][start_idx:start_idx+random_length] for dim in range(0,dims)]
+            shapelets.append(single_shapelet)
+
+    return shapelets
+
+
 def get_distance(time_series, shapelet): # distance between univariate time series and shapelet
     max_idx = len(time_series) - len(shapelet)
     min_dist = float('inf')
