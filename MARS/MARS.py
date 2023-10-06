c8051ca8e51083cf61cc12cfb6c9d356cb15f0a4
Mario Bianchi <bianchi.mario@outlook.com>
2023-10-06 15:20:04 +0200

    Updated transform with indexes

--- MARS/MARS.py
+++ MARS/MARS.py
@@ -77,66 +77,9 @@ def get_random_shapelets(time_series_dataset, num_shapelets, max_len, min_len, a
     return shapelets
 
 
-def get_random_shapelets_and_indexes(time_series_dataset, num_shapelets, max_len, min_len, async_limit=None, seed=None):
-    if seed is not None:
-        random.seed(seed)
-
-    dims = len(time_series_dataset[0])
-    max_possible_length = min([len(e) for e in time_series_dataset[0]]) # length of the shortest dimension
-
-    if (max_len>max_possible_length) or (min_len>max_possible_length):
-        raise ValueError("Shapelet length is greater than the length of the shortest dimension.")
-    
-    shapelets = []
-    indexes_list = []
-    random_ts = random.sample(range(0, len(time_series_dataset)), num_shapelets) # indexes of the time series that generate a shapelet
-
-    for idx in random_ts:
-        ts = time_series_dataset[idx]
-
-        if async_limit==None:
-            single_shapelet = []
-            single_shapelet_indexes = []
-            random_length = random.randint(min_len,max_len)
-            for dim in range(0,dims): # async: new starting point for each dimension
-                start_idx = random.randint(0,len(ts[dim]) - random_length)
-                single_shapelet.append(ts[dim][start_idx:start_idx+random_length])
-                single_shapelet_indexes.append(start_idx)
-            shapelets.append(single_shapelet)
-            indexes_list.append(single_shapelet_indexes)
-
-        elif async_limit>0:
-            single_shapelet = []
-            single_shapelet_indexes = []
-            random_length = random.randint(min_len,max_len)
-            min_start_idx = random.randint(0, max_len - random_length)
-            max_start_idx = min_start_idx + async_limit
-            for dim in range(0,dims):
-                start_idx = random.randint(min_start_idx, max_start_idx)
-                single_shapelet.append(ts[dim][start_idx:start_idx+random_length])
-                single_shapelet_indexes.append(start_idx)
-            shapelets.append(single_shapelet)
-            indexes_list.append(single_shapelet_indexes)
-
-        else: # async_limit<=0
-            random_length = random.randint(min_len,max_len)
-            start_idx = random.randint(0, max_len - random_length) # index from zero to last possible position
-            single_shapelet = [ts[dim][start_idx:start_idx+random_length] for dim in range(0,dims)]
-            single_shapelet_indexes = [start_idx]
-            shapelets.append(single_shapelet)
-            indexes_list.append(single_shapelet_indexes)
-
-    return shapelets, indexes_list
-
-
-def fit(time_series_dataset, num_shapelets, max_len, min_len, async_limit=None, save_indexes=False, seed=None):
-    if save_indexes==False:
-        output = get_random_shapelets(time_series_dataset, num_shapelets, max_len, min_len, async_limit, seed)
-        return output
-    
-    else:
-        output,indexes = get_random_shapelets_and_indexes(time_series_dataset, num_shapelets, max_len, min_len, async_limit, seed)
-        return output,indexes
+def fit(time_series_dataset, num_shapelets, max_len, min_len, async_limit=None, seed=None):
+    output = get_random_shapelets(time_series_dataset, num_shapelets, max_len, min_len, async_limit, seed)
+    return output
 
 
 # ---------------------------------- Calculating distances ----------------------------------
@@ -163,15 +106,56 @@ def get_distance_multi(multivariate_time_series, multivariate_shapelet):
     return tot_dist
 
 
-def transform(time_series_dataset, shapelets):
+def get_distance_and_index(time_series, shapelet):
+    shapelet_len = len(shapelet)
+    
+    # Create a sliding window view of the time_series for efficient calculation
+    time_series_windows = np.lib.stride_tricks.sliding_window_view(time_series, shapelet_len)
+    
+    # Calculate distances for all windows simultaneously
+    distances = np.linalg.norm(time_series_windows - shapelet, axis=1)
+    
+    min_dist = np.min(distances) # get mimimum
+    min_dist_idx = np.argmin(distances) # get index
+
+    return min_dist,min_dist_idx
+
+
+def get_distance_multi_and_indexes(multivariate_time_series, multivariate_shapelet):
+    tot_dist = 0
+    indexes_list = [] # list of indexes where there is the mimimum dist. between shapelet and MTS
+    for dim in range(len(multivariate_shapelet)):
+        dim_dist,idx = get_distance_and_index(multivariate_time_series[dim], multivariate_shapelet[dim])
+        tot_dist += dim_dist
+        indexes_list.append(idx)
+    return tot_dist,indexes_list
+
+
+def transform(time_series_dataset, shapelets, indexes=False):
     distances_dataset = []
+    indexes_dataset = []
 
-    for idx,ts in enumerate(time_series_dataset):
-        print('Calculating distances for TS #', idx)
-        ts_distances = []
-        for shapelet in shapelets:
-            dist = get_distance_multi(ts, shapelet)
-            ts_distances.append(dist)
-        distances_dataset.append(ts_distances)
+    if indexes==False:
+        for idx,ts in enumerate(time_series_dataset):
+            print('Calculating distances for TS #', idx)
+            ts_distances = []
+            for shapelet in shapelets:
+                dist = get_distance_multi(ts, shapelet)
+                ts_distances.append(dist)
+            distances_dataset.append(ts_distances)
 
-    return distances_dataset
+        return distances_dataset
+    
+    else: # save indexes
+        for idx,ts in enumerate(time_series_dataset):
+            print('Calculating distances for TS #', idx)
+            ts_distances = []
+            ts_indexes = []
+            for shapelet in shapelets:
+                dist,indexes = get_distance_multi_and_indexes(ts, shapelet)
+                ts_distances.append(dist)
+                ts_indexes.append(indexes)
+            distances_dataset.append(ts_distances)
+            indexes_dataset.append(ts_indexes)
+
+        return distances_dataset,indexes_dataset
